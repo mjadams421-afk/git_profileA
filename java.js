3f331906d58f0e564f489a9ad06645114a11dc0b
Mat <mjadams421@gmail.com>
2026-09-07 18:36:18 +0000

    Added a few formating changes

--- java.js
+++ java.js
@@ -11,8 +11,8 @@ function action() {
  let name = window.prompt('What is your name?');
  let reason = window.prompt('What is the reason for your visit?');
  if(name && reason) {
-  answer += `Hello my name is ${name} and I am visiting because ${reason}.`;
- } else { answer += `Please enter your name and reason for visit.`; }
+  answer = `Hello my name is ${name} and I am visiting because ${reason}.`;
+ } else { answer = `Please enter your name and reason for visit.`; }
  footer.style.backgroundColor = `rgb(${random}, ${random}, ${random})`;
  response.innerHTML = answer;
  }
